7d82fe2ad91629a5b216e083b96c9434516da7e4
lakshyaagraw <lakshyaagrawal775@gmail.com>
2024-08-10 02:10:32 +0530

    Day 1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program
+{
+
+     static void Main(string[] args)
+     {
+
+          /*
+          Console.WriteLine("Hello World!");
+
+
+          variables 
+          int a = 3;
+          double pi = 3.14;
+          char c = 'a';
+          string hello = "Hello World!";
+          bool f = true;
+
+          Console.WriteLine(a);
+          Console.WriteLine(pi);
+          Console.WriteLine(c);
+          Console.WriteLine(hello);
+          Console.WriteLine(f);
+
+          //    Display Variables
+
+          string name = "Programmer";
+
+          Console.WriteLine("Hello " + name);
+
+          int a = 300;
+          long myNum = 15000000000L;
+          float my = 5.57F;
+          double pi = 3.144444444444D;
+          bool isCharpfun = true;
+          char myGrade = 'b';
+          string name = "Hello";
+    
+
+       // Implicit Casting
+          int myInt = 9;
+          double myDouble = myInt;
+     // Explicit casting
+          double myDouble2 = 9.78;
+          int myInt2 = (int)myDouble2;
+
+      // User input
+        Console.WriteLine("Enter the your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hi "+ name);
+       
+       Console.WriteLine("Enter the age ");
+       int age = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("Your age is : " + age);
+
+     //   Assignement Operator
+       int num1 = 10;
+         num1 = 5;
+       Console.WriteLine(num1);
+         num1 += 5;
+       Console.WriteLine(num1);
+         num1 -= 5;
+       Console.WriteLine(num1);
+         num1 *= 5;
+       Console.WriteLine(num1);
+         num1 /= 5;
+       Console.WriteLine(num1);
+         num1 %= 5;
+       Console.WriteLine(num1);
+         num1 &= 5;
+       Console.WriteLine(num1);
+         num1 |= 5;
+       Console.WriteLine(num1);
+         num1 ^= 5;
+       Console.WriteLine(num1);
+         num1 >>= 5;
+       Console.WriteLine(num1);
+         num1 <<= 5;
+       Console.WriteLine(num1);
+
+     */
+     }
+}
